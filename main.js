2dfed6780b2de95ca3f9549d8fd3729dca2ce530
JacobIsho <jisho@uwo.ca>
2025-04-27 19:45:43 -0400

    Disabled the ability to resize and fullscreen the window

--- main.js
+++ main.js
@@ -6,6 +6,8 @@ function createWindow() {
     mainWindow = new BrowserWindow({
         width: 800,
         height: 600,
+        fullscreenable: false,
+        resizable: false,
         webPreferences: {
             nodeIntegration: true
         }
