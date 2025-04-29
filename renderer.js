b9e0d12c415ec248a647ed8218ebf443c48ef2f0
AdamsApple05 <23nn@queensu.ca>
2025-04-28 22:07:44 -0400

    added timer

--- renderer.js
+++ renderer.js
@@ -1,3 +1,52 @@
+let startTime;
+let timerInterval;
+let isRunning = false;
+let isReset = true;
+
+function pad(number) {
+    return (number < 10 ? "0" : "") + number;
+}
+
+function displayTime(milliseconds) {
+    const minutes = Math.floor(milliseconds / (60000));
+    const seconds = Math.floor((milliseconds % (60000))/1000);
+    const centiseconds = Math.floor((milliseconds % 1000)/10);
+
+    const formattedTime = `${pad(minutes)}:${pad(seconds)}:${pad(centiseconds)}`;
+    document.getElementById('timer').textContent = formattedTime;
+}
+
+function startTimer() {
+    startTime = Date.now();
+    timerInterval = setInterval(updateTimer, 10);
+    isRunning = true;
+    isReset = false;
+}
+
+function updateTimer() {
+    const elapsedTime = Date.now() - startTime;
+    displayTime(elapsedTime);
+}
+
+function pauseTimer(){
+    clearInterval(timerInterval);
+    isRunning = false;
+}
+
+function resetTimer(){
+    clearInterval(timerInterval);
+    isRunning = false;
+    displayTime(0);
+    isReset = true;
+}
+
+
 document.getElementById('testButton').addEventListener('click', () => {
-    alert('Button clicked!');
+    if(isReset){
+        startTimer();
+    }else if(isRunning && !isReset){
+        pauseTimer();
+    }else{
+        resetTimer();
+    }
 });
